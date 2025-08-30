72695bf28d70fe3746a15895b8df7c8a8ab3d9f6
AS200585 <ananaysri2005@gmail.com>
2025-08-31 00:38:28 +0530

    commit 3: Added more examples for plotting with Matplotlib

--- Linear-Algebra/matplotlib_base.py
+++ Linear-Algebra/matplotlib_base.py
@@ -13,4 +13,19 @@ plt.show()
 x2 = np.arange(-4, 5)
 y2 = x2**4 + x2*-2
 plt.plot(x2, y2, 'r')
+plt.plot(x2, y2/2, 'gs') # for additional points on same graph
 plt.show()
+
+# Example 3 - Plotting a Straight Line only
+plt.plot([-2, 5], [-1, 1], 'k-', label='first line')
+plt.plot([-1, 0], [-3, 2], 'g-', label='second line')
+plt.legend() # Add a legend to show which line is which using the labels and colors
+plt.show()
+
+# Example 4 - Visualize Images
+img = np.random.randint(-3, 19, size=(8, 9))
+print(img)
+plt.imshow(img)
+plt.colorbar() # Show a color scale
+plt.show() 
+# This displays the image with the color scale that corresponds to the values of each element of the matrix as colors
