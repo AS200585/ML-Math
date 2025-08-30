7d9fd869ffbdc190ff799d9d924ebd12f13c09b4
AS200585 <ananaysri2005@gmail.com>
2025-08-31 01:16:13 +0530

    commit 3-1: Add vectors and scalar multiplication concepts

---
+++ Linear-Algebra/vectors-and-scalar-multiplication.py
@@ -0,0 +1,40 @@
+# Vectors and Scalar Multiplication
+
+"""
+Vectors is a mathematical object that has both magnitude and direction. 
+Vectors are also called and ordered lists of numbers.
+They are often represented as arrows in a coordinate system, with the length of the arrow indicating 
+the magnitude and the direction of the arrow indicating the direction.
+These arrows can be added together or multiplied by a scalar to produce new vectors.
+
+Vectors can be represented in component form, such as (x, y, z), where each component corresponds to a dimension in space.
+These components can be manipulated using various mathematical operations, such as addition, subtraction, and scalar multiplication.
+Vectors can be represented in matrix form, where each column represents a different vector.
+Vectors can also be represented as a single column or row in a matrix, depending on the context.
+
+Vectors can have different orientations - they can be column vectors or row vectors.
+Column vectors are represented as a single column of values, while row vectors are represented as a single row of values. 
+This is the algebraic interpretation is important when performing operations like matrix multiplication.
+There is also a geometric interpretation of vectors, which considers their direction and magnitude in space.
+This geometric interpretation is important for understanding concepts like vector addition and scalar multiplication.
+
+Vector-Scalar Multiplication: W = λ x v(vector)
+When Lambda (λ) is a scalar and v is a vector, the result of multiplying them is a new vector W.
+λ > 1 means stretching the vector in the direction of its components, while λ < 1 means compressing it.
+λ = 1 means the vector remains unchanged, and λ = 0 results in the zero vector.
+λ < 0 means the vector is flipped in the opposite direction.
+
+In AIML, vectors are used to represent various types of data, such as word embeddings, image features, and more. 
+They play a crucial role in machine learning algorithms, particularly in the areas of natural language processing and computer vision.
+"""
+"""
+Dimensionality is the number of components or coordinates needed to specify a point in a space.
+It also refers to the number of features or attributes in a dataset, the number of columns in a data table, the number of dimensions 
+in a geometric space, and also the number of elements in a vector.
+
+In the context of vectors, it refers to the number of dimensions in which the vector exists.
+For example, a 2D vector has two components (x, y), while a 3D vector has three components (x, y, z).
+
+Higher-dimensional vectors can exist in spaces with more than three dimensions, which are often used in machine learning and data analysis.
+"""
+
